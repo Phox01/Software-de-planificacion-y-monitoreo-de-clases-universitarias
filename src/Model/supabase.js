78621cd505d967d9469b8c2d49512f4d9ba8942d
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2025-08-02 23:34:36 -0400

    📄Insert de datos

--- src/Model/supabase.js
+++ src/Model/supabase.js
@@ -4,3 +4,16 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://cpxtnnyhylslptljkoai.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNweHRubnloeWxzbHB0bGprb2FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNzY2ODksImV4cCI6MjA2OTc1MjY4OX0.6Na5Yr2zBiiRb1jNCyGJYMwT6cf8CQ96tpWz9iQNMp4"
 export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export async function insertData(table, data) {
+  const { data: insertedData, error } = await supabase
+    .from(table)
+    .insert(data)
+
+  if (error) {
+    console.error('Error inserting data:', error)
+    throw error
+  }
+
+  return insertedData
+}
