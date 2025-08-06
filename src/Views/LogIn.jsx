b12dc2569fd31b43c32024ac803fc9274b5bb598
Joseph Ruiz <josephruiz.molina@gmail.com>
2025-08-05 20:09:18 -0400

    🗃️ ready forms and styles of singup and login

--- src/Views/LogIn.jsx
+++ src/Views/LogIn.jsx
@@ -1,33 +1,71 @@
-import React from 'react'
-import styles from './CSS/LogIn.module.css'
+import { useForm } from "react-hook-form";
+import styles from "./CSS/LogIn.module.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function LogIn() {
+  useEffect(() => {
+    document.body.classList.add(styles.authContainer);
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-        <div>
-            <h2>L</h2>
-            <h1 className={styles.title}>Inicia Sesion</h1>
-            <h2 className={styles.subtitle}>Ingresa tu correo y tu contraseña para iniciar</h2>
-        </div>
-        <div className={styles.card}>
-            <button className={styles.input}>Continua con Google</button>
-            <h2 className={styles.subtitle}>O inicia sesión</h2>
-            <input type="email" placeholder="Correo electrónico" className={styles.input}/>
-            <input type="password" placeholder="Contraseña" className={styles.input}/>
-            <div className={styles.passwordInfo}>
-                <label className={styles.subtitle}>
-                    <input type="checkbox" className={styles.checkbox}/>
-                    Recordarme
-                </label>
-                <h2 className={styles.subtitle}>¿Olvidaste la contraseña?</h2>
-            </div>
-            <button className={styles.mainButton}>Iniciar Sesion</button>
-            <h3 className={styles.subtitle}>
-              ¿No tienes una cuenta? 
-              <Link to="/signup"> Regístrate</Link>
-            </h3>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.upperSection}>
+        <h2>L</h2>
+        <h1 className={styles.title}>Inicia Sesion</h1>
+        <h2 className={styles.subtitle}>
+          Ingresa tu correo y tu contraseña para iniciar
+        </h2>
+      </div>
+      <div className={styles.card}>
+        <button className={styles.input}>Continua con Google</button>
+        <h2 className={styles.subtitle}>O inicia sesión</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("email", {
+              required: "El correo es obligatorio",
+            })}
+            placeholder="Correo electrónico"
+            className={styles.input}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+
+          <input
+            {...register("contraseña", {
+              required: "La contraseña es obligatoria",
+            })}
+            placeholder="Contraseña"
+            className={styles.input}
+          />
+          {errors.contraseña && <p>{errors.contraseña.message}</p>}
+
+          <div className={styles.passwordInfo}>
+            <label className={styles.subtitle}>
+              <input type="checkbox" className={styles.checkbox} />
+              Recordarme
+            </label>
+            <h2 className={styles.subtitle}>¿Olvidaste la contraseña?</h2>
+          </div>
+          <button type="submit" className={styles.mainButton}>
+            Iniciar Sesion
+          </button>
+        </form>
+
+        <h3 className={styles.subtitle}>
+          ¿No tienes una cuenta?
+          <Link to="/signup"> Regístrate</Link>
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
--- src/Views/SignUp.jsx
+++ src/Views/SignUp.jsx
@@ -1,29 +1,87 @@
-import React from 'react'
-import styles from './CSS/SignUp.module.css'
+import styles from "./CSS/LogIn.module.css";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function SignUp() {
+  useEffect(() => {
+    document.body.classList.add(styles.authContainer);
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-        <div>
-            <h2>L</h2>
-            <h1 className={styles.title}>Regístrate</h1>
-            <h2 className={styles.subtitle}>Regístrate con tu correo y una contraseña</h2>
-        </div>
-        <div className={styles.card}>
-            <button className={styles.input}>Regístrate con Google</button>
-            <h2 className={styles.subtitle}>O inicia sesión</h2>
-            <input type="email" placeholder="Número de carnet" className={styles.input}/>
-            <input type="email" placeholder="Nombre" className={styles.input}/>
-            <input type="email" placeholder="Apellido" className={styles.input}/>
-            <input type="email" placeholder="Correo electrónico" className={styles.input}/>
-            <input type="password" placeholder="Contraseña" className={styles.input}/>
-            <button className={styles.mainButton}>Regístrarse</button>
-            <h3 className={styles.subtitle}>
-              ¿Tienes una cuenta? 
-              <Link to="/login"> Inicia Sesión</Link>
-            </h3>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.upperSection}>
+        <h2>L</h2>
+        <h1 className={styles.title}>Regístrate</h1>
+        <h2 className={styles.subtitle}>
+          Regístrate con tu correo y una contraseña
+        </h2>
+      </div>
+      <div className={styles.card}>
+        <button className={styles.input}>Regístrate con Google</button>
+        <h2 className={styles.subtitle}>O inicia sesión</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            {...register("carnet", {
+              required: "El n° de carnet es obligatorio",
+            })}
+            placeholder="Número de carnet"
+            className={styles.input}
+          />
+          {errors.carnet && <p>{errors.carnet.message}</p>}
+          <input
+            {...register("nombre", {
+              required: "El nomrbe es obligatorio",
+            })}
+            placeholder="Nombre"
+            className={styles.input}
+          />
+          {errors.nombre && <p>{errors.nombre.message}</p>}
+          <input
+            {...register("apellido", {
+              required: "El apellido es obligatorio",
+            })}
+            placeholder="Apellido"
+            className={styles.input}
+          />
+          {errors.apellido && <p>{errors.apellido.message}</p>}
+          <input
+            {...register("email", {
+              required: "El email es obligatorio",
+            })}
+            placeholder="Correo electrónico"
+            className={styles.input}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+          <input
+            {...register("contraseña", {
+              required: "La contraseña es obligatoria",
+            })}
+            placeholder="Contraseña"
+            className={styles.input}
+          />
+          {errors.contraseña && <p>{errors.contraseña.message}</p>}
+          <button type="submit" className={styles.mainButton}>
+            Regístrarse
+          </button>
+        </form>
+
+        <h3 className={styles.subtitle}>
+          ¿Tienes una cuenta?
+          <Link to="/login"> Inicia Sesión</Link>
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
